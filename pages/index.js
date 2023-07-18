0e877c0b686ab22cea75869ff701d966bec93d26
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-07-19 00:28:07 +0300

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Heading from "../components/Heading";
 //import Image from 'next/image';
-import s from '../styles/index.module.css';
+import s from '../styles/Index.module.css';
 import { Typography } from "@mui/material";
 
 
