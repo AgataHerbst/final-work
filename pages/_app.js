4e23d44ae528ae727fafe9bb1d004b8a7064ae95
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-07-27 21:07:27 +0300

    new commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import { SessionProvider, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { SessionProvider } from 'next-auth/react';
+//import { useRouter } from 'next/router';
+import { useState } from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Basket from '../components/Basket';
@@ -10,20 +10,20 @@ import '../styles/globals.css';
 
 const theme = createTheme({
     palette: {
-      primary: {
-        light: '#757ce8',
-        main: '#FFE4B5',
-        dark: '#002884',
-        contrastText: '#fff',
-      },
-      secondary: {
-        light: '#ff7961',
-        main: '#FF8C00',
-        dark: '#ba000d',
-        contrastText: '#000',
-      },
+        primary: {
+            light: '#757ce8',
+            main: '#FFE4B5',
+            dark: '#002884',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#FF8C00',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
     },
-  });
+});
 
 function MyApp({ Component,
     pageProps: { session, ...pageProps }
@@ -37,22 +37,13 @@ function MyApp({ Component,
     console.log(Component.auth)
     return <>
         <SessionProvider session={session}>
-        <ThemeProvider theme={theme}> 
-            <Navbar
-                handleCart={setCartOpen}
-                orderLen={10}
-            />
-             </ThemeProvider>
-            <main>
-                {Component.auth ? (
-                    <Auth>
-                        <Component {...pageProps} />
-                    </Auth>
-                ) : (
-                    <Component {...pageProps} />
-                )
-                }
-            </main>
+            <ThemeProvider theme={theme}>
+                <Navbar
+                    handleCart={setCartOpen}
+                    orderLen={10}
+                />
+            </ThemeProvider>
+            <Component {...pageProps} />
             <Container fixed >
                 <Footer />
             </Container>
@@ -63,33 +54,12 @@ function MyApp({ Component,
                 closeCart={() => setCartOpen(false)}
             />
         </SessionProvider>
-
-    </>
+        </>
 }
 export default MyApp;
 
-function Auth({ children }) {
-    const router = useRouter()
-    const { data: session, status } = useSession()
-    const isUser = !!session?.user
-    const loading = status === 'loading'
 
-    useEffect(() => {
-        if (!loading) {
-            if (!isUser) {
-                router.push('/api/auth/signin')
-            }
-        }
-    }, [isUser, loading])
 
-    if (loading) {
-        return <h3>Loading...</h3>
-    }
+   
 
-    if (!loading && isUser) {
 
-        return <>{children}</>
-    }
-    return null
-
-}
