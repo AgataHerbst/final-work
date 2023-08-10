17c06280644080077de7ac6d3e7c2e25e73887c5
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-08-11 00:29:48 +0300

    new commit

--- components/SocialMediaIcons.js
+++ components/SocialMediaIcons.js
@@ -7,27 +7,18 @@ const SocialMediaIcons = () => {
   return (
     <div>
      <Link
-        className="hover:opacity-50 transition duration-500"
         href="https://t.me/+HXDREgaKVyk2MzQy"
-        target="_blank"
-        rel="noreferrer"
-      >
+        >
        <TelegramIcon sx={{ fontSize: 40 }}/>
       </Link>
       <Link
-        className="hover:opacity-50 transition duration-500"
         href="https://chat.whatsapp.com/LwsrjwGOS7aLcqeOEHpJyj"
-        target="_blank"
-        rel="noreferrer"
-      >
+       >
         <WhatsAppIcon sx={{ fontSize: 40 }} />
       </Link>
       <Link
-        className="hover:opacity-50 transition duration-500"
-        href="https://instagram.com/bakery_irsh?igshid=NjIwNzIyMDk2Mg=="
-        target="_blank"
-        rel="noreferrer"
-      >
+       href="https://instagram.com/bakery_irsh?igshid=NjIwNzIyMDk2Mg=="
+        >
         <InstagramIcon sx={{ fontSize: 40 }} />
       </Link>
     </div>
